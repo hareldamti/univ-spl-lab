9e1807a17c17efcfb1b52ff137830133dc026a3b
hareldamti <hd3501@gmail.com>
2023-06-12 10:27:42 +0300

    task 2 - whole file mmap

---
+++ loader.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+int foreach_phdr(void *map_start, void (*func)(Elf32_Phdr *,int), int arg) {
+    Elf32_Ehdr* elf_hdr = (Elf32_Ehdr*) map_start;
+    for (int i = 0; i < elf_hdr -> e_phnum; i++) {
+        Elf32_Phdr* phdr = (Elf32_Phdr*) (map_start + elf_hdr -> e_phoff + i * sizeof(Elf32_Phdr));
+        func(phdr, arg);
+    }
+}
+
+int print_cntr = 0;
+void print(Elf32_Phdr * phdr, int arg) {
+    printf("Program header number %d is in address %x\n", print_cntr, phdr -> p_offset);
+    print_cntr++;
+}
+
+void phdr_table(Elf32_Phdr * phdr, int arg) {
+    char flags[3] = {'R', 'W', 'E'};
+    for (int i = 0; i < 3; i++) if (!((phdr -> p_flags >> (2-i)) & 1)) flags[i] = ' ';
+    printf("%-9d\t%x\t%x\t\t%x\t\t%x\t%x\t%s\t%x\n",
+        phdr -> p_type,
+        phdr -> p_offset,
+        phdr -> p_vaddr,
+        phdr -> p_paddr,
+        phdr -> p_filesz,
+        phdr -> p_memsz,
+        flags,
+        phdr -> p_align
+    );
+    
+}
+
+void load_phdr(Elf32_Phdr * phdr, int fd) {
+    if (phdr -> p_type != 1) return;
+    int permissions = (phdr -> p_flags & 4)
+                    + (phdr -> p_flags & 2) * 2
+                    + (phdr -> p_flags & 1) * 4;
+    
+    Elf32_Off vaddr   = phdr -> p_vaddr  & 0xfffff000,
+              offset  = phdr -> p_offset & 0xfffff000,
+              padding = phdr -> p_vaddr   & 0xfff;
+
+    mmap( (void*)vaddr, phdr -> p_memsz + padding, permissions, MAP_PRIVATE | MAP_FIXED, fd, offset);
+    phdr_table(phdr, 0);
+}
+
+int main(int argc, char ** argv) {
+    if (argc < 2) exit(1);
+    int fd = open(argv[1], 'r');
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) perror("Couldn't get file size");
+
+    void* map = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    //////// task0:
+    // foreach_phdr(map, print, 0);
+
+    //////// task1:
+    printf("Type\t\toffset\tVirtAddr\tPhysAddr\tFileSiz\tMemSiz\tFlg\tAlign\n");
+    foreach_phdr(map, phdr_table, 0);
+
+    //////// task2:
+    foreach_phdr(map, load_phdr, fd);
+    close(fd);
+
+}
--- makefile
+++ makefile
@@ -1,12 +1,13 @@
-name = task0
-
 all: clean compile link
 
 clean:
-	rm -f *.o $(name)
+	rm -f *.o loader
 
 compile:
-	nasm -g -f elf32 $(name).s -o $(name).o
+	gcc -m32 -c loader.c -o loader.o
+	nasm -g -f elf32 start.s -o start.o
+	nasm -g -f elf32 startup.s -o startup.o
+	
 
 link:
-	gcc -no-pie -g -m32 $(name).o -o $(name) 
+	ld -o loader loader.o startup.o start.o -L/usr/lib32 -lc -T linking_script -dynamic-linker /lib32/ld-linux.so.2
